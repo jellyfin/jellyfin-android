2555dfe19e58011f7fb11b292673b1aae3428f2d
Cromefire_ <tim.langhorst@outlook.de>
2020-07-28 19:09:31 +0200

    Simplified NativeShell API (#2)
    Simplified API and AppHost

--- app/src/main/assets/www/native/apphost.js
+++ app/src/main/assets/www/native/apphost.js
@@ -1,107 +1,76 @@
-var deviceId;
-var deviceName;
-var appName;
-var appVersion;
-
-var features = [
-    'filedownload',
-    'displaylanguage',
+const features = [
+    "filedownload",
+    "displaylanguage",
     //'externalplayerintent',
-    'subtitleappearancesettings',
+    "subtitleappearancesettings",
     //'sharing',
-    'exit',
-    'htmlaudioautoplay',
-    'htmlvideoautoplay',
-    'externallinks',
-    'multiserver',
-    'physicalvolumecontrol',
-    'remotecontrol',
-    'castmenuhashchange'
+    "exit",
+    "htmlaudioautoplay",
+    "htmlvideoautoplay",
+    "externallinks",
+    "multiserver",
+    "physicalvolumecontrol",
+    "remotecontrol",
+    "castmenuhashchange"
 ];
 
 function getDeviceProfile(profileBuilder, item) {
-    var profile = profileBuilder({
+    const profile = profileBuilder({
         enableMkvProgressive: false
     });
 
     profile.CodecProfiles = profile.CodecProfiles.filter(function (i) {
-        return i.Type == 'Audio';
+        return i.Type === "Audio";
     });
 
     profile.SubtitleProfiles.push(
         {
-            Format: 'ssa',
-            Method: 'External'
+            Format: "ssa",
+            Method: "External"
         },
         {
-            Format: 'ass',
-            Method: 'External'
+            Format: "ass",
+            Method: "External"
         }
     );
 
     profile.CodecProfiles.push({
-        Type: 'Video',
-        Container: 'avi',
+        Type: "Video",
+        Container: "avi",
         Conditions: [
             {
-                Condition: 'NotEqual',
-                Property: 'CodecTag',
-                Value: 'xvid'
+                Condition: "NotEqual",
+                Property: "CodecTag",
+                Value: "xvid"
             }
         ]
     });
 
     profile.CodecProfiles.push({
-        Type: 'Video',
-        Codec: 'h264',
+        Type: "Video",
+        Codec: "h264",
         Conditions: [
-        {
-            Condition: 'EqualsAny',
-            Property: 'VideoProfile',
-            Value: 'high|main|baseline|constrained baseline'
-        },
-        {
-            Condition: 'LessThanEqual',
-            Property: 'VideoLevel',
-            Value: '41'
-        }]
+            {
+                Condition: "EqualsAny",
+                Property: "VideoProfile",
+                Value: "high|main|baseline|constrained baseline"
+            },
+            {
+                Condition: "LessThanEqual",
+                Property: "VideoLevel",
+                Value: "41"
+            }]
     });
 
     profile.TranscodingProfiles.reduce(function (profiles, p) {
-        if (p.Type == 'Video' && p.CopyTimestamps == true && p.VideoCodec == 'h264') {
-            p.AudioCodec += ',ac3';
+        if (p.Type === "Video" && p.CopyTimestamps === true && p.VideoCodec === "h264") {
+            p.AudioCodec += ",ac3";
             profiles.push(p);
         }
         return profiles;
     }, []);
 
     return profile;
-};
-
-function getDeviceProfileForVideo(item) {
-    var container = item.Container;
-    var videoTracks = audioTracks = subtitleTracks = [];
-
-    for (var i = 0; i < item.MediaStreams.lengh; i++) {
-        var track = item.MediaStreams[i];
-
-        switch (track.Type) {
-            case 'Video':
-                videoTracks.push(parseVideoTrack(track));
-                break;
-            case 'Audio':
-                audioTracks.push(parseAudioTrack(track));
-                break;
-            case 'Subtitle':
-                subtitleTracks.push(parseSubtitleTrack(track));
-                break;
-            default:
-                continue;
-        }
-    }
-
-    var supportedTracks = window.ExoPlayer.checkTracksSupport(container, videoTracks, audioTracks, subtitleTracks);
-    // TODO: check if the given tracks are supported. If not, they are not added up to directPlayProfiles
 }
 
 function parseVideoTrack(track) {
@@ -129,55 +98,82 @@ function parseSubtitleTrack(track) {
     };
 }
 
+function getDeviceProfileForVideo(item) {
+    const container = item.Container;
+    const videoTracks = [];
+    const audioTracks = [];
+    const subtitleTracks = [];
+
+    for (let i = 0; i < item.MediaStreams.length; i++) {
+        const track = item.MediaStreams[i];
+
+        switch (track.Type) {
+            case "Video":
+                videoTracks.push(parseVideoTrack(track));
+                break;
+            case "Audio":
+                audioTracks.push(parseAudioTrack(track));
+                break;
+            case "Subtitle":
+                subtitleTracks.push(parseSubtitleTrack(track));
+                break;
+        }
+    }
+
+    const supportedTracks = window.ExoPlayer.checkTracksSupport(container, videoTracks, audioTracks, subtitleTracks);
+    // TODO: check if the given tracks are supported. If not, they are not added up to directPlayProfiles
+}
+
+let deviceId;
+let deviceName;
+let appName;
+let appVersion;
+
 window.NativeShell.AppHost = {
-    exit: function () {
+    exit() {
         if (navigator.app && navigator.app.exitApp) {
             navigator.app.exitApp();
         } else {
             window.close();
         }
     },
-    supports: function (command) {
-        return features.indexOf(command.toLowerCase()) != -1;
+    supports(command) {
+        return features.includes(command.toLowerCase());
     },
     getSyncProfile: getDeviceProfile,
-    getDefaultLayout: function() {
-        return 'mobile';
+    getDefaultLayout() {
+        return "mobile";
     },
-    getDeviceProfile: getDeviceProfile,
-    init: function() {
-        return new Promise(function(resolve, reject) {
-            window.NativeShell.getDeviceInformation(function(result) {
-                // set globally so they can be used elsewhere
-                deviceId = result.deviceId;
-                deviceName = result.deviceName;
-                appName = result.appName;
-                appVersion = result.appVersion;
+    getDeviceProfile,
+    init() {
+        try {
+            const result = window.NativeInterface.getDeviceInformation();
+            // set globally so they can be used elsewhere
+            deviceId = result.deviceId;
+            deviceName = result.deviceName;
+            appName = result.appName;
+            appVersion = result.appVersion;
 
-                appInfo = {
-                    deviceId: deviceId,
-                    deviceName: deviceName,
-                    appName: appName,
-                    appVersion: appVersion
-                };
-
-                resolve(appInfo);
-            }, function(err) {
-                console.log(err);
-                reject();
+            return Promise.resolve({
+                deviceId,
+                deviceName,
+                appName,
+                appVersion,
             });
-        });
+        } catch (e) {
+            return Promise.reject(e);
+        }
     },
-    deviceName: function() {
+    deviceName() {
         return deviceName;
     },
-    deviceId: function() {
+    deviceId() {
         return deviceId;
     },
-    appName: function() {
+    appName() {
         return appName;
     },
-    appVersion: function() {
+    appVersion() {
         return appVersion;
     }
 };
--- app/src/main/assets/www/native/nativeshell.js
+++ app/src/main/assets/www/native/nativeshell.js
@@ -1,85 +1,33 @@
-function NativeShell() {}
+window.NativeShell = {
+    enableFullscreen: NativeInterface.enableFullscreen,
+    disableFullscreen: NativeInterface.disableFullscreen,
+    openUrl: NativeInterface.openUrl,
+    hideMediaSession: NativeInterface.hideMediaSession,
 
-NativeShell.prototype.getDeviceInformation = function(successCallback, errorCallback) {
-    var result = window.NativeInterface.getDeviceInformation();
-    if (result) {
-        successCallback && successCallback(JSON.parse(result));
-    } else {
-        errorCallback && errorCallback();
+    updateMediaSession(mediaInfo) {
+        window.NativeInterface.updateMediaSession(JSON.stringify(mediaInfo));
+    },
+
+    downloadFile(downloadInfo) {
+        window.NativeInterface.downloadFile(JSON.stringify(downloadInfo));
+    },
+
+    getPlugins() {
+        return [];
+    },
+
+    execCast(action, args, callback) {
+        this.castCallbacks = this.castCallbacks || {};
+        this.castCallbacks[action] = callback;
+        window.NativeInterface.execCast(action, JSON.stringify(args));
+    },
+
+    castCallback(action, keep, err, result) {
+        const callbacks = this.castCallbacks || {};
+        const callback = callbacks[action];
+        callback && callback(err || null, result);
+        if (!keep) {
+            delete callbacks[action];
+        }
     }
 };
-
-NativeShell.prototype.enableFullscreen = function(successCallback, errorCallback) {
-    var result = window.NativeInterface.enableFullscreen();
-    if (result) {
-        successCallback && successCallback();
-    } else {
-        errorCallback && errorCallback();
-    }
-};
-
-NativeShell.prototype.disableFullscreen = function(successCallback, errorCallback) {
-    var result = window.NativeInterface.disableFullscreen();
-    if (result) {
-        successCallback && successCallback();
-    } else {
-        errorCallback && errorCallback();
-    }
-};
-
-NativeShell.prototype.openUrl = function(url, target, successCallback, errorCallback) {
-    var result = window.NativeInterface.openIntent(url);
-    if (result) {
-        successCallback && successCallback();
-    } else {
-        errorCallback && errorCallback();
-    }
-};
-
-NativeShell.prototype.updateMediaSession = function(options, successCallback, errorCallback) {
-    var result = window.NativeInterface.updateMediaSession(JSON.stringify(options));
-    if (result) {
-        successCallback && successCallback();
-    } else {
-        errorCallback && errorCallback();
-    }
-};
-
-NativeShell.prototype.hideMediaSession = function(successCallback, errorCallback) {
-    var result = window.NativeInterface.hideMediaSession();
-    if (result) {
-        successCallback && successCallback();
-    } else {
-        errorCallback && errorCallback();
-    }
-};
-
-NativeShell.prototype.downloadFile = function(options, successCallback, errorCallback) {
-    var result = window.NativeInterface.downloadFile(JSON.stringify(options));
-    if (result) {
-        successCallback && successCallback();
-    } else {
-        errorCallback && errorCallback();
-    }
-};
-
-NativeShell.prototype.getPlugins = function() {
-    return [];
-};
-
-NativeShell.prototype.execCast = function(action, args, callback) {
-    this.castCallbacks = this.castCallbacks || {};
-    this.castCallbacks[action] = callback;
-    window.NativeInterface.execCast(action, JSON.stringify(args));
-}
-
-NativeShell.prototype.castCallback = function(action, keep, err, result) {
-    var callbacks = this.castCallbacks || {};
-    var callback = callbacks[action];
-    callback && callback(err || null, result);
-    if (!keep) {
-        delete callbacks[action];
-    }
-}
-
-window.NativeShell = new NativeShell();
