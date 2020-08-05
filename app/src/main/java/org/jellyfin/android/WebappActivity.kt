69b5db81e4407aa3c0c44154ae8764bf57622fab
Maxr1998 <max.rumpf1998@gmail.com>
2020-08-05 10:54:10 +0200

    Set WebView background color to prevent white flash on startup

--- app/src/main/java/org/jellyfin/android/WebappActivity.kt
+++ app/src/main/java/org/jellyfin/android/WebappActivity.kt
@@ -14,6 +14,7 @@ import android.provider.Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import org.jellyfin.android.bridge.NativeInterface
 import org.jellyfin.android.cast.Chromecast
 import org.jellyfin.android.utils.lazyView
@@ -45,6 +46,7 @@ class WebappActivity : AppCompatActivity(), WebViewController {
 
         // Setup WebView
         setContentView(R.layout.activity_webapp)
+        webView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary))
         webView.webChromeClient = WebChromeClient()
         webView.settings.apply {
             javaScriptEnabled = true
