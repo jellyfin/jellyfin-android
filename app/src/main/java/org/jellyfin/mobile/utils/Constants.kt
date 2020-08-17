180379bfd047c0bfdae9e62a65e71758b4a1c941
Maxr1998 <max.rumpf1998@gmail.com>
2020-08-17 18:36:33 +0200

    Refactor constants

--- app/src/main/java/org/jellyfin/mobile/utils/Constants.kt
+++ app/src/main/java/org/jellyfin/mobile/utils/Constants.kt
@@ -1,6 +1,10 @@
 package org.jellyfin.mobile.utils
 
 object Constants {
+    // Webapp constants
+    const val INDEX_PATCH_PATH = "index_patch.html"
+    const val INDEX_PATH = "web/index.html"
+
     // Preference keys
     const val PREF_INSTANCE_URL = "pref_instance_url"
     const val PREF_IGNORE_BATTERY_OPTIMIZATIONS = "pref_ignore_battery_optimizations"
@@ -8,17 +12,11 @@ object Constants {
     const val PREF_MUSIC_NOTIFICATION_ALWAYS_DISMISSIBLE = "pref_music_notification_always_dismissible"
     const val PREF_ENABLE_EXOPLAYER = "pref_enable_exoplayer"
 
-    // Misc
-    const val INDEX_PATCH_PATH = "index_patch.html"
-    const val INDEX_PATH = "web/index.html"
+    // Intent extras
     const val EXTRA_MEDIA_SOURCE_ITEM = "org.jellyfin.mobile.MEDIA_SOURCE_ITEM"
     const val EXTRA_WEBAPP_MESSENGER = "org.jellyfin.mobile.WEBAPP_MESSENGER"
-    const val LANGUAGE_UNDEFINED = "und"
-    const val TICKS_PER_MILLISECOND = 10000
-    const val PLAYER_TIME_UPDATE_RATE = 3000L
-    const val DEFAULT_SEEK_TIME_MS = 5000L
 
-    // Player actions
+    // Music player actions
     const val ACTION_PLAY = "action_play"
     const val ACTION_PAUSE = "action_pause"
     const val ACTION_REWIND = "action_rewind"
@@ -29,7 +27,13 @@ object Constants {
     const val ACTION_REPORT = "action_report"
     const val ACTION_SHOW_PLAYER = "ACTION_SHOW_PLAYER"
 
-    // Player events
+    // Video player constants
+    const val LANGUAGE_UNDEFINED = "und"
+    const val TICKS_PER_MILLISECOND = 10000
+    const val PLAYER_TIME_UPDATE_RATE = 3000L
+    const val DEFAULT_SEEK_TIME_MS = 5000L
+
+    // Video player events
     const val EVENT_PLAYING = "Playing"
     const val EVENT_PAUSE = "Pause"
     const val EVENT_ENDED = "Ended"
