1f7a036a31542f65b18e816d3350d965601c6dff
Maxr1998 <max.rumpf1998@gmail.com>
2020-08-30 13:02:23 +0200

    Show toast to restart app when changing ExoPlayer setting

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -27,7 +27,7 @@ object Dependencies {
         const val constraintLayout = "1.1.3"
         const val webkitX = "1.2.0"
         const val coil = "0.11.0"
-        const val modernAndroidPreferences = "1.0-RC1"
+        const val modernAndroidPreferences = "1.0-RC2"
 
         // Cast
         const val mediaRouter = "1.1.0"
