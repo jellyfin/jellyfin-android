c1d2fe2ef24d9671ce172bcd24bb147ba8a94d0a
Niels van Velzen <git@ndat.nl>
2020-10-18 12:25:15 +0200

    Update Gradle Build Tools version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         val kotlinVersion: String by project
-        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("com.android.tools.build:gradle:4.1.0")
         classpath(kotlin("gradle-plugin", kotlinVersion))
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.6.2.0")
     }
