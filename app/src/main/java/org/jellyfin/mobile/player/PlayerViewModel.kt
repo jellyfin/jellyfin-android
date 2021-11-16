33e97c085b62710cd3a893bb544bff709b3b5a8a
Maxr1998 <max.rumpf1998@gmail.com>
2021-11-16 15:04:13 +0100

    Fix potential crash when loading display preferences

--- app/src/main/java/org/jellyfin/mobile/player/PlayerViewModel.kt
+++ app/src/main/java/org/jellyfin/mobile/player/PlayerViewModel.kt
@@ -133,6 +133,9 @@ class PlayerViewModel(application: Application) : AndroidViewModel(application),
                 )
             } catch (e: ApiClientException) {
                 Timber.e(e, "Failed to load display preferences")
+            } catch (e: IllegalArgumentException /* kotlinx.serialization.SerializationException */) {
+                // TODO: Remove catch branch when SDK is updated to >=10.8 based API
+                Timber.e(e, "Failed to load display preferences")
             }
         }
 
