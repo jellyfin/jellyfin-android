d9d41db7b138d2a856be971bdd1b1e1c1e91eff7
Maxr1998 <max.rumpf1998@gmail.com>
2022-09-23 01:10:41 +0200

    Decouple NativePlayer from WebViewFragment

--- app/src/main/java/org/jellyfin/mobile/bridge/NativePlayerHost.kt
+++
@@ -1,7 +0,0 @@
-package org.jellyfin.mobile.bridge
-
-import org.jellyfin.mobile.player.interaction.PlayOptions
-
-interface NativePlayerHost {
-    fun loadNativePlayer(playOptions: PlayOptions)
-}
--- app/src/main/java/org/jellyfin/mobile/events/ActivityEvent.kt
+++ app/src/main/java/org/jellyfin/mobile/events/ActivityEvent.kt
@@ -1,10 +1,12 @@
 package org.jellyfin.mobile.events
 
 import android.net.Uri
+import org.jellyfin.mobile.player.interaction.PlayOptions
 import org.json.JSONArray
 
 sealed class ActivityEvent {
     class ChangeFullscreen(val isFullscreen: Boolean) : ActivityEvent()
+    class LaunchNativePlayer(val playOptions: PlayOptions) : ActivityEvent()
     class OpenUrl(val uri: String) : ActivityEvent()
     class DownloadFile(val uri: Uri, val title: String, val filename: String) : ActivityEvent()
     class CastMessage(val action: String, val args: JSONArray) : ActivityEvent()
