a10c54a1632cdc319794d7cde8797c777fa4fc55
Susan Thapa <33973551+susonthapa@users.noreply.github.com>
2022-11-26 03:59:26 +0545

    Add back button in the integrated player (#901)
    * added back button in the exo player
    
    * fixed the margin on the left of the title
    
    * Fix whitespace issues
    
    * Use resource reference for color
    
    * Move toolbar button handling to onViewCreated
    
    Co-authored-by: Maxr1998 <max.rumpf1998@gmail.com>

--- app/src/main/java/org/jellyfin/mobile/player/ui/PlayerFragment.kt
+++ app/src/main/java/org/jellyfin/mobile/player/ui/PlayerFragment.kt
@@ -16,8 +16,8 @@ import android.view.ViewGroup
 import android.view.WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_NONE
 import android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
 import android.widget.ImageButton
-import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.isVisible
 import androidx.core.view.updatePadding
@@ -57,7 +57,7 @@ class PlayerFragment : Fragment() {
     private var _playerControlsBinding: ExoPlayerControlViewBinding? = null
     private val playerControlsBinding: ExoPlayerControlViewBinding get() = _playerControlsBinding!!
     private val playerControlsView: View get() = playerControlsBinding.root
-    private val titleTextView: TextView get() = playerControlsBinding.trackTitle
+    private val toolbar: Toolbar get() = playerControlsBinding.toolbar
     private val fullscreenSwitcher: ImageButton get() = playerControlsBinding.fullscreenSwitcher
     private var playerMenus: PlayerMenus? = null
 
@@ -110,7 +110,7 @@ class PlayerFragment : Fragment() {
             }
 
             // Update title and player menus
-            titleTextView.text = jellyfinMediaSource.name
+            toolbar.title = jellyfinMediaSource.name
             playerMenus?.onQueueItemChanged(queueItem)
         }
 
@@ -157,6 +157,9 @@ class PlayerFragment : Fragment() {
             )
             insets
         }
+
+        // Handle toolbar back button
+        toolbar.setNavigationOnClickListener { parentFragmentManager.popBackStack() }
 
         // Create playback menus
         playerMenus = PlayerMenus(this, playerBinding, playerControlsBinding)
