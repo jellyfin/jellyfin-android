cea6224153935309950cd0e3e7d84dba39d469ff
Maxr1998 <max.rumpf1998@gmail.com>
2022-12-28 13:51:14 +0100

    Rework fullscreen/insets handling and fix deprecations

--- app/src/main/java/org/jellyfin/mobile/utils/extensions/Activity.kt
+++ app/src/main/java/org/jellyfin/mobile/utils/extensions/Activity.kt
@@ -1,38 +1,11 @@
-@file:Suppress("DEPRECATION")
-
 package org.jellyfin.mobile.utils.extensions
 
 import android.app.Activity
 import android.content.pm.ActivityInfo
 import android.graphics.Point
 import android.view.Surface
-import android.view.View
-import android.view.WindowManager
 
-const val STABLE_LAYOUT_FLAGS = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-
-const val FULLSCREEN_FLAGS = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-    View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-
-fun Activity.isFullscreen() = window.decorView.systemUiVisibility.hasFlag(FULLSCREEN_FLAGS)
-
-fun Activity.enableFullscreen() {
-    window.apply {
-        decorView.systemUiVisibility = FULLSCREEN_FLAGS
-        addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
-    }
-}
-
-fun Activity.disableFullscreen(keepStableLayout: Boolean = false) {
-    window.apply {
-        decorView.systemUiVisibility = if (keepStableLayout) STABLE_LAYOUT_FLAGS else 0
-        clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-    }
-}
-
+@Suppress("DEPRECATION")
 fun Activity.lockOrientation() {
     val display = windowManager.defaultDisplay
     val size = Point().also(display::getSize)
