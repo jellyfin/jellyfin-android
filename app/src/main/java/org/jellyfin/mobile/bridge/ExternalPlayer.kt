5453acf283a753cae9adf2b6d109d0b7f5759f44
Niels van Velzen <nielsvanvelzen@users.noreply.github.com>
2023-05-17 21:50:22 +0200

    Fix bitrate not set for external player profile (#1073)
    (cherry picked from commit 1b640f34fd7d74cac399bfaa99212e55b00ff1af)

--- app/src/main/java/org/jellyfin/mobile/bridge/ExternalPlayer.kt
+++ app/src/main/java/org/jellyfin/mobile/bridge/ExternalPlayer.kt
@@ -101,6 +101,7 @@ class ExternalPlayer(
                 startTimeTicks = playOptions.startPositionTicks,
                 audioStreamIndex = playOptions.audioStreamIndex,
                 subtitleStreamIndex = playOptions.subtitleStreamIndex,
+                maxStreamingBitrate = playOptions.maxBitrate,
             ).onSuccess { jellyfinMediaSource ->
                 playMediaSource(playOptions, jellyfinMediaSource)
             }.onFailure { error ->
