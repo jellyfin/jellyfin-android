ab8fe7c3db14f6261cfc976a9c3636a028f9b0e8
CeruleanRed <toni.suta@gmail.com>
2025-01-01 12:07:45 +0100

    Update chapter markings when video progress changes

--- app/src/main/java/org/jellyfin/mobile/player/ui/PlayerMenus.kt
+++ app/src/main/java/org/jellyfin/mobile/player/ui/PlayerMenus.kt
@@ -13,7 +13,6 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.get
 import androidx.core.view.isVisible
-import androidx.core.view.marginStart
 import androidx.core.view.size
 import androidx.core.view.updateLayoutParams
 import androidx.core.view.updateLayoutParams
