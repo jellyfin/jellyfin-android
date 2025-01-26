70d3cf6317b692df13797726e510f446cd3994d4
Jakob Kukla <jakob.kukla@gmail.com>
2025-01-26 02:44:12 +0100

    Update jellyfin-sdk-kotlin to v1.6.1 (#1506)
    * Update dependency org.jellyfin.sdk:jellyfin-core to v1.6.1
    
    Fix build errors and add API authorization headers to exoplayer
    since the access token is no longer passed via URL for Android Auto.
    See https://github.com/jellyfin/jellyfin-sdk-kotlin/pull/871
    
    * Use SDK for authorization header string building
    
    * Use getItem in MediaSourceResolver
    
    * Don't send authorization header to origins other than jellyfin server
    
    * Fix detekt lint warnings

--- app/src/main/java/org/jellyfin/mobile/player/source/MediaSourceResolver.kt
+++ app/src/main/java/org/jellyfin/mobile/player/source/MediaSourceResolver.kt
@@ -3,10 +3,10 @@ package org.jellyfin.mobile.player.source
 import org.jellyfin.mobile.player.PlayerException
 import org.jellyfin.sdk.api.client.ApiClient
 import org.jellyfin.sdk.api.client.exception.ApiClientException
-import org.jellyfin.sdk.api.client.extensions.itemsApi
 import org.jellyfin.sdk.api.client.extensions.mediaInfoApi
-import org.jellyfin.sdk.api.operations.ItemsApi
+import org.jellyfin.sdk.api.client.extensions.userLibraryApi
 import org.jellyfin.sdk.api.operations.MediaInfoApi
+import org.jellyfin.sdk.api.operations.UserLibraryApi
 import org.jellyfin.sdk.model.api.DeviceProfile
 import org.jellyfin.sdk.model.api.PlaybackInfoDto
 import org.jellyfin.sdk.model.serializer.toUUIDOrNull
@@ -15,7 +15,7 @@ import java.util.UUID
 
 class MediaSourceResolver(private val apiClient: ApiClient) {
     private val mediaInfoApi: MediaInfoApi = apiClient.mediaInfoApi
-    private val itemsApi: ItemsApi = apiClient.itemsApi
+    private val userLibraryApi: UserLibraryApi = apiClient.userLibraryApi
 
     @Suppress("ReturnCount")
     suspend fun resolveMediaSource(
@@ -34,7 +34,6 @@ class MediaSourceResolver(private val apiClient: ApiClient) {
             val response by mediaInfoApi.getPostedPlaybackInfo(
                 itemId = itemId,
                 data = PlaybackInfoDto(
-                    userId = apiClient.userId,
                     // We need to remove the dashes so that the server can find the correct media source.
                     // And if we didn't pass the mediaSourceId, our stream indices would silently get ignored.
                     // https://github.com/jellyfin/jellyfin/blob/9a35fd673203cfaf0098138b2768750f4818b3ab/Jellyfin.Api/Helpers/MediaInfoHelper.cs#L196-L201
@@ -60,8 +59,7 @@ class MediaSourceResolver(private val apiClient: ApiClient) {
 
         // Load additional item info if possible
         val item = try {
-            val response by itemsApi.getItemsByUserId(ids = listOf(itemId))
-            response.items?.firstOrNull()
+            userLibraryApi.getItem(itemId).content
         } catch (e: ApiClientException) {
             Timber.e(e, "Failed to load item for media source $itemId")
             null
