5763a6e758b14a8dcd2f1fcd5f8e254f550ef051
CeruleanRed <toni.suta@gmail.com>
2025-05-25 18:24:13 +0200

    Fix some linting issues - Rename Millisecond to Ms to stay in line with other constant names

--- app/src/main/java/org/jellyfin/mobile/utils/TickUtils.kt
+++ app/src/main/java/org/jellyfin/mobile/utils/TickUtils.kt
@@ -1,9 +1,7 @@
 package org.jellyfin.mobile.utils
 
-class TickUtils {
-    companion object{
-        fun ticksToMs(ticks: Long) = ticks / Constants.TICKS_PER_MILLISECOND
-        fun msToTicks(ms: Long) = ms * Constants.TICKS_PER_MILLISECOND
-        fun secToTicks(sec: Long) = msToTicks(sec * 1000)
-    }
+object TickUtils {
+    fun ticksToMs(ticks: Long) = ticks / Constants.TICKS_PER_MS
+    fun msToTicks(ms: Long) = ms * Constants.TICKS_PER_MS
+    fun secToTicks(sec: Long) = msToTicks(sec * Constants.MS_PER_SEC)
 }
