56cd71d29ed38d0f98f383fca94dcb3dd252abf8
Niels van Velzen <git@ndat.nl>
2026-05-26 15:28:56 +0200

    Add route metadata to media item ids

--- app/src/main/java/org/jellyfin/mobile/sessionbrowser/LibraryService.kt
+++ app/src/main/java/org/jellyfin/mobile/sessionbrowser/LibraryService.kt
@@ -2,7 +2,6 @@ package org.jellyfin.mobile.sessionbrowser
 
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
-import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaLibraryService
 import androidx.media3.session.MediaSession
@@ -15,41 +14,40 @@ class LibraryService : MediaLibraryService() {
     private val apiClientController: ApiClientController by inject()
     private val apiClient: ApiClient by inject()
 
-    private var mediaLibrarySession: MediaLibrarySession? = null
-    private var callback: MediaLibrarySession.Callback? = null
-
     private val playerAudioAttributes = AudioAttributes.Builder()
         .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
         .setUsage(C.USAGE_MEDIA)
         .build()
 
-    private val exoPlayer: Player by lazy {
+    private val exoPlayer: ExoPlayer by lazy {
         ExoPlayer.Builder(this).build().apply {
             setAudioAttributes(playerAudioAttributes, true)
             setHandleAudioBecomingNoisy(true)
         }
     }
 
+    private val mediaLibrarySession: MediaLibrarySession by lazy {
+        MediaLibrarySession.Builder(this, exoPlayer, callback)
+            .build()
+    }
+
+    private val callback: MediaLibrarySession.Callback by lazy {
+        SessionBrowserCallback(this, apiClient)
+    }
+
     override fun onGetSession(
         controllerInfo: MediaSession.ControllerInfo,
-    ): MediaLibrarySession? = mediaLibrarySession
+    ): MediaLibrarySession = mediaLibrarySession
 
     override fun onCreate() {
         super.onCreate()
 
         runBlocking { apiClientController.loadSavedServerUser() }
-
-        if (callback == null) callback = SessionBrowserCallback(this, apiClient)
-        mediaLibrarySession = MediaLibrarySession.Builder(this, exoPlayer, callback!!)
-            .build()
     }
 
     override fun onDestroy() {
-        mediaLibrarySession?.run {
-            player.release()
-            release()
-            mediaLibrarySession = null
-        }
+        exoPlayer.release()
+        mediaLibrarySession.release()
 
         super.onDestroy()
     }
