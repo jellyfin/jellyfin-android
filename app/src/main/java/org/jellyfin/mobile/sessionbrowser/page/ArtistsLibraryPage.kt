08e455959c03db54b2e959e80a3fe5e4ba094c7e
Niels van Velzen <git@ndat.nl>
2026-05-27 10:08:14 +0200

    Use artists API for querying artists in Android Auto

--- app/src/main/java/org/jellyfin/mobile/sessionbrowser/page/ArtistsLibraryPage.kt
+++ app/src/main/java/org/jellyfin/mobile/sessionbrowser/page/ArtistsLibraryPage.kt
@@ -5,18 +5,15 @@ import org.jellyfin.mobile.sessionbrowser.LibraryPageElement
 import org.jellyfin.mobile.sessionbrowser.LibraryRoute
 import org.jellyfin.mobile.sessionbrowser.libraryPage
 import org.jellyfin.sdk.api.client.ApiClient
-import org.jellyfin.sdk.api.client.extensions.itemsApi
-import org.jellyfin.sdk.model.api.BaseItemKind
+import org.jellyfin.sdk.api.client.extensions.artistsApi
 import org.jellyfin.sdk.model.api.ImageType
 import org.jellyfin.sdk.model.api.ItemSortBy
 
 val ArtistsLibraryPage = { api: ApiClient ->
     libraryPage<LibraryRoute.Artists>(grid = true) { route, offset, limit ->
-        val result by api.itemsApi.getItems(
+        val result by api.artistsApi.getArtists(
             parentId = route.libraryId,
-            includeItemTypes = listOf(BaseItemKind.MUSIC_ARTIST),
             sortBy = listOf(ItemSortBy.SORT_NAME),
-            recursive = true,
             imageTypeLimit = 1,
             enableImageTypes = listOf(ImageType.PRIMARY),
             nameLessThan = if (route.startLetter == ALPHA_BROWSER_OTHER) ALPHA_BROWSER_LETTERS[0].toString() else null,
