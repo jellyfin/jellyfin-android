7a93079d3acafd9b48d3e0d5ba81a7498a2eaf2e
Niels van Velzen <nielsvanvelzen@users.noreply.github.com>
2026-07-25 14:16:27 +0200

    Revert "Remove automatic landscape mode"
    This reverts commit b9db6945292507a02c8b3d02faad18344c25e570.

--- app/src/main/java/org/jellyfin/mobile/MainActivity.kt
+++ app/src/main/java/org/jellyfin/mobile/MainActivity.kt
@@ -8,6 +8,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.os.IBinder
 import android.provider.Settings
+import android.view.OrientationEventListener
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.SystemBarStyle
@@ -31,6 +32,7 @@ import org.jellyfin.mobile.utils.BackPressInterceptor
 import org.jellyfin.mobile.utils.BluetoothPermissionHelper
 import org.jellyfin.mobile.utils.Constants
 import org.jellyfin.mobile.utils.PermissionRequestHelper
+import org.jellyfin.mobile.utils.SmartOrientationListener
 import org.jellyfin.mobile.utils.extensions.replaceFragment
 import org.jellyfin.mobile.utils.isWebViewSupported
 import org.jellyfin.mobile.webapp.RemotePlayerService
@@ -58,6 +60,8 @@ class MainActivity : AppCompatActivity() {
             serviceBinder = null
         }
     }
+
+    private val orientationListener: OrientationEventListener by lazy { SmartOrientationListener(this) }
 
     /**
      * Passes back press events onto the currently visible [Fragment] if it implements the [BackPressInterceptor] interface.
@@ -141,6 +145,11 @@ class MainActivity : AppCompatActivity() {
         chromecast.initializePlugin(this)
     }
 
+    override fun onStart() {
+        super.onStart()
+        orientationListener.enable()
+    }
+
     private fun handleServerState(state: ServerState) {
         with(supportFragmentManager) {
             val currentFragment = findFragmentById(R.id.fragment_container)
@@ -188,6 +197,11 @@ class MainActivity : AppCompatActivity() {
                 fragment.onUserLeaveHint()
             }
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        orientationListener.disable()
     }
 
     override fun onDestroy() {
