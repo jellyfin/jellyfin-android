5ce80438aaa249cc4b8d8639ffe14f27bb3c3068
Rogal <progalin55@gmail.com>
2026-09-22 12:06:33 +0200

    Wrap toolbar title instead of cropping it

--- app/src/main/java/org/jellyfin/mobile/player/ui/PlayerFragment.kt
+++ app/src/main/java/org/jellyfin/mobile/player/ui/PlayerFragment.kt
@@ -15,6 +15,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_NONE
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
@@ -73,6 +74,7 @@ class PlayerFragment : Fragment(), BackPressInterceptor {
     private val playerControlsBinding: ExoPlayerControlViewBinding get() = _playerControlsBinding!!
     private val playerControlsView: View get() = playerControlsBinding.root
     private val toolbar: Toolbar get() = playerControlsBinding.toolbar
+    private val toolbarTitle: TextView get() = playerControlsBinding.toolbarTitle
     private val fullscreenSwitcher: ImageButton get() = playerControlsBinding.fullscreenSwitcher
     private var playerMenus: PlayerMenus? = null
 
@@ -129,7 +131,7 @@ class PlayerFragment : Fragment(), BackPressInterceptor {
             }
 
             // Update title and player menus
-            toolbar.title = mediaSource.getName(requireContext())
+            toolbarTitle.text = mediaSource.getName(requireContext())
             playerMenus?.onQueueItemChanged(mediaSource, viewModel.queueManager.hasNext())
         }
 
